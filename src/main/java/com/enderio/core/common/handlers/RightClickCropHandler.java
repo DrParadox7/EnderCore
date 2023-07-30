d1b1225db7fa4012e28b8c677d3d6c3bafbb11bb
DrParadox7 <gustavo.caliari@live.com>
2023-07-31 00:08:28 +0200

    Right click to harvest only works with Sickles but are AoE

--- src/main/java/com/enderio/core/common/handlers/RightClickCropHandler.java
+++ src/main/java/com/enderio/core/common/handlers/RightClickCropHandler.java
@@ -1,9 +1,13 @@
 package com.enderio.core.common.handlers;
 
 import java.util.List;
+import java.util.Random;
 
 import net.minecraft.block.Block;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.util.FakePlayer;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.Action;
 import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent;
@@ -14,6 +18,9 @@ import com.enderio.core.common.config.ConfigHandler;
 import com.enderio.core.common.util.ItemUtil;
 import com.google.common.collect.Lists;
 
+import cofh.core.item.tool.ItemSickleAdv;
+import cofh.lib.util.helpers.MathHelper;
+import cofh.redstonearsenal.item.tool.ItemSickleRF;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 
@@ -49,7 +56,7 @@ public class RightClickCropHandler {
     private List<PlantInfo> plants = Lists.newArrayList();
 
     private PlantInfo currentPlant = null;
-
+    private static final Random rnd = new Random();
     public static final RightClickCropHandler INSTANCE = new RightClickCropHandler();
 
     private RightClickCropHandler() {}
@@ -63,18 +70,41 @@ public class RightClickCropHandler {
         int x = event.x, y = event.y, z = event.z;
         Block block = event.world.getBlock(x, y, z);
         int meta = event.world.getBlockMetadata(x, y, z);
+        ItemStack stack = event.entityPlayer.getHeldItem();
         if (ConfigHandler.allowCropRC && event.action == Action.RIGHT_CLICK_BLOCK
-                && (event.entityPlayer.getHeldItem() == null || !event.entityPlayer.isSneaking())) {
+                && !(event.entityPlayer instanceof FakePlayer)
+                && stack != null
+                && stack.getUnlocalizedName().toLowerCase().contains("sickle")) {
             for (PlantInfo info : plants) {
                 if (info.blockInst == block && meta == info.meta) {
                     if (event.world.isRemote) {
                         event.entityPlayer.swingItem();
                     } else {
-                        currentPlant = info;
-                        block.dropBlockAsItem(event.world, x, y, z, meta, 0);
-                        currentPlant = null;
-                        event.world.setBlockMetadataWithNotify(x, y, z, info.resetMeta, 3);
-                        event.setCanceled(true);
+                        int range = 3;
+                        boolean specialDurability = false;
+
+                        // Thermal Foundation Sickle
+                        if (stack.getItem() instanceof ItemSickleAdv) {
+                            ItemSickleAdv Sickle = (ItemSickleAdv) event.entityPlayer.getHeldItem().getItem();
+                            range = Sickle.radius;
+                        }
+                        // Redstone Arsenal Sickle
+                        if (stack.getItem() instanceof ItemSickleRF) {
+                            specialDurability = true;
+                            if (!canDoEnergyOperations(stack)) range = 1;
+                        }
+                        for (int i = x - range; i <= x + range; i++) {
+                            for (int k = z - range; k <= z + range; k++) {
+                                currentPlant = info;
+                                block.dropBlockAsItem(event.world, i, y, k, meta, 0);
+                                currentPlant = null;
+                                event.world.setBlockMetadataWithNotify(i, y, k, info.resetMeta, 3);
+                                event.setCanceled(true);
+                            }
+                        }
+                        if (!specialDurability) {
+                            event.entityPlayer.getHeldItem().damageItem(1, event.entityPlayer);
+                        }
                     }
                     break;
                 }
@@ -95,5 +125,24 @@ public class RightClickCropHandler {
                 }
             }
         }
+    }
+
+    public boolean canDoEnergyOperations(ItemStack stack) {
+        ItemSickleRF sickle = (ItemSickleRF) stack.getItem();
+
+        // RF Math variables
+        boolean isEmpowered = sickle.isEmpowered(stack);
+        int unbreakingLevel = MathHelper
+                .clamp((EnchantmentHelper.getEnchantmentLevel(Enchantment.unbreaking.effectId, stack)), 0, 4);
+        int costRF = sickle.energyPerUse * (5 - unbreakingLevel) / 5;
+
+        if (sickle.getEnergyStored(stack) > costRF) {
+            sickle.extractEnergy(
+                    stack,
+                    isEmpowered ? sickle.energyPerUseCharged * (5 - unbreakingLevel) / 5
+                            : sickle.energyPerUse * (5 - unbreakingLevel) / 5,
+                    false);
+            return true;
+        } else return false;
     }
 }
